ce8033c713ada393f1abcff69254051845698185
Дмитрий <agent1935@gmail.com>
2023-08-08 17:18:16 +0300

    add File_01

--- Json.py
+++ Json.py
@@ -1,13 +1,12 @@
 import json
 
 student_data = {
-
-123456: (37),
-147852: ('Dmytro'),
-258741: ('A country', 'Ukraine'),
-369852: ('Kyiv'),
-369147: ('Year of birth', 1986),
+    123456: {'Возраст': 37},
+    147852: {'Имя': 'Dmytro'},
+    258741: {'A country': 'Ukraine'},
+    369852: {'Kyiv': None},  # Уточните, что здесь ожидается
+    369147: {'Year of birth': 1986},
 }
+
 with open('task_01.json', 'w') as f:
-    json.dump(student_data, f)
-    print(student_data)
+    json.dump(student_data, f, indent=4, ensure_ascii=False)
